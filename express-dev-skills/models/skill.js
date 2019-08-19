93cd945672649838e27af2d2f2912dc2b73c7169
Braxton Toomer <braxton.toomer@gmail.com>
2019-08-18 23:12:10 -0700

    Set up folders and working on routes

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,30 @@
+//Data Resource
+
+const skills = [
+  {
+    name: 'HTML',
+    description:  'Markup language used to describe structure of a web page.',
+    experience: '3 months'
+  },
+  {
+    name: 'CSS',
+    description:  'Stylesheet language used to style HTML displayed in a webpage.',
+    experience: '3 months'
+  },
+  {
+    name: 'JavaScript',
+    description: 'Programming language used to make dynamic webpages and add interactivity.',
+    experience: '2 months'
+  },
+  {
+    name: 'Node.js and Express',
+    description: 'Run time environment that uses modules like Express that creates a server and allows you to executing JS outside of a web browser.',
+    experience: '1 week'
+  },
+  {
+    name: 'Mongoose',
+    description: 'Node package that allows you to perform CRUD operations on a MongoDB database.',
+    experience: '1 week'
+  }
+];
+
--- express-dev-skills/routes/index.js
+++ express-dev-skills/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const skillsCtrl = require('../controllers/skills');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
